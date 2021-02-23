4ab4e625d291f6f710a711755bd854972d7513c2
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-23 21:00:09 +0800

    Add files via upload

---
+++ leetcode_no1052_爱生气的书店老板_w1.cpp
@@ -0,0 +1,81 @@
+﻿/* ------------------------------------------------------------------|
+今天，书店老板有一家店打算试营业 customers.length 分钟。每分钟都有一些顾客（customers[i]）会进入书店，所有这些顾客都会在那一分钟结束后离开。
+
+在某些时候，书店老板会生气。 如果书店老板在第 i 分钟生气，那么 grumpy[i] = 1，否则 grumpy[i] = 0。 当书店老板生气时，那一分钟的顾客就会不满意，不生气则他们是满意的。
+
+书店老板知道一个秘密技巧，能抑制自己的情绪，可以让自己连续 X 分钟不生气，但却只能使用一次。
+
+请你返回这一天营业下来，最多有多少客户能够感到满意的数量。
+ 
+
+示例：
+
+输入：customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], X = 3
+输出：16
+解释：
+书店老板在最后 3 分钟保持冷静。
+感到满意的最大客户数量 = 1 + 1 + 1 + 1 + 7 + 5 = 16.
+ 
+
+提示：
+
+1 <= X <= customers.length == grumpy.length <= 20000
+0 <= customers[i] <= 1000
+0 <= grumpy[i] <= 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/grumpy-bookstore-owner
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：32 ms, 在所有 C++ 提交中击败了93.53%的用户
+*	内存消耗：29.3 MB, 在所有 C++ 提交中击败了86.61%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int X) {
+    int n = customers.size();
+    int res = 0;
+    int tmp = 0;
+    int max_people = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!grumpy[i]) {
+            res += customers[i];
+        }
+
+        if (i < X) {
+            tmp += customers[i] * grumpy[i];
+            max_people = tmp;
+        }
+        else {
+            tmp = tmp - customers[i - X] * grumpy[i - X] + customers[i] * grumpy[i];
+            max_people = max(max_people, tmp);
+        }
+    }
+    return res + max_people;
+}
